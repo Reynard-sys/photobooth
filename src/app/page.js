d03d44de77f8fc3735941995af308b1b13abd8c0
Rey <reynard.john.rabanal@gmail.com>
2026-02-04 04:11:32 +0800

    initial landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,81 @@
 import Image from "next/image";
+import Border from "../components/border";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <Border />
+      <main className="flex min-h-screen flex-col items-center p-15 sm:p-18 md:p-12 lg:p-24 pt-25 sm:pt-0 md:pt-5 lg:pt-30 bg-[#FDFDF5]">
+        {/* Small Screen Size */}
+        <div className="flex lg:hidden inset-0 items-center justify-center pointer-events-none z-10 pt-20 mb-8">
+          <Image
+            src="/mobile_booth.png"
+            alt="Photobooth Small"
+            width={600}
+            height={300.948}
+            priority
+            className="pointer-events-auto w-[70vw] max-w-140 h-auto"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+
+        <button className="lg:hidden relative group">
+          <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-1 translate-y-1"></div>
+          <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 px-6 hover:bg-[#E6CFCF] transition-colors">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/def_start.png"
+              alt="Start Button"
+              width={49}
+              height={15}
+              priority
+              className="pointer-events-auto w-[49px] sm:w-[120px] md:w-[150px] h-auto"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+        </button>
+
+        {/* Middle Screen Size */}
+        <div className="hidden lg:flex xl:hidden fixed inset-y-0 left-25 items-center justify-center pointer-events-none z-10">
+          <Image
+            src="/pc_title.png"
+            alt="Photobooth Large"
+            width={529.829}
+            height={162.723}
+            priority
+            className="pointer-events-auto w-[40vw] max-w-530 h-auto"
+          />
+        </div>
+        <div className="hidden lg:flex xl:hidden fixed inset-y-0 right-25 items-center justify-center pointer-events-none z-10">
+          <Image
+            src="/pc_booth.png"
+            alt="Photobooth Large"
+            width={554.039}
+            height={621.22}
+            priority
+            className="pointer-events-auto w-[40vw] max-w-554 h-auto"
+          />
+        </div>
+
+        {/* Large Screen Size */}
+        <div className="hidden xl:flex fixed inset-y-0 left-25 items-center justify-center pointer-events-none z-10">
+          <Image
+            src="/pc_title.png"
+            alt="Photobooth Large"
+            width={800}
+            height={162.723}
+            priority
+            className="pointer-events-auto w-[40vw] max-w-800 h-auto"
+          />
+        </div>
+        <div className="hidden xl:flex fixed inset-y-0 right-25 items-center justify-center pointer-events-none z-10">
+          <Image
+            src="/pc_booth.png"
+            alt="Photobooth Large"
+            width={600}
+            height={621.22}
+            priority
+            className="pointer-events-auto w-[40vw] max-w-600 h-auto"
+          />
         </div>
       </main>
-    </div>
+    </>
   );
 }
---
+++ src/components/border.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Border() {
+  return (
+    <div className="fixed inset-0 border-25 sm:border-20 lg:border-40 pointer-events-none z-50 border-[#3D568F]">
+      <div className="fixed inset-3 sm:inset-3 lg:inset-8 border-10 sm:border-10 lg:border-10 rounded-3xl pointer-events-none z-50 border-[#3D568F]"></div>
+    </div>
+  );
+}
