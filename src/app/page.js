46c848b67391339884061f444e06f8fde7fc6602
Rey <reynard.john.rabanal@gmail.com>
2026-02-04 14:04:48 +0800

    Added button and improved responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Border from "../components/border";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,31 +18,60 @@ export default function Home() {
             className="pointer-events-auto w-[70vw] max-w-140 h-auto"
           />
         </div>
-
-        <button className="lg:hidden relative group">
-          <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-1 translate-y-1"></div>
-          <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 px-6 hover:bg-[#E6CFCF] transition-colors">
+        <Link href="/timer" className="lg:hidden relative group">
+          <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-1 sm:translate-x-2 translate-y-1 sm:translate-y-2 group-active:bg-[#F2AEBD] transition-colors"></div>
+          <div className="relative bg-[#F2DDDC] border-1 sm:border-2 border-[#3D568F] rounded-xl py-3 sm:py-3 md:py-4 px-7 sm:px-10 md:px-12 group-active:bg-[#3D568F] group-active:border-[#F2AEBD] transition-colors">
             <Image
               src="/def_start.png"
               alt="Start Button"
               width={49}
               height={15}
               priority
-              className="pointer-events-auto w-[49px] sm:w-[120px] md:w-[150px] h-auto"
+              className="pointer-events-none w-[14vw] sm:w-[14vw] md:w-[12vw] h-auto group-active:hidden"
+            />
+            <Image
+              src="/hovered_start.png"
+              alt="Start Button"
+              width={200}
+              height={15}
+              priority
+              className="pointer-events-none w-[14vw] sm:w-[14vw] md:w-[12vw] h-auto hidden group-active:block"
             />
           </div>
-        </button>
-
+        </Link>
         {/* Middle Screen Size */}
         <div className="hidden lg:flex xl:hidden fixed inset-y-0 left-25 items-center justify-center pointer-events-none z-10">
-          <Image
-            src="/pc_title.png"
-            alt="Photobooth Large"
-            width={529.829}
-            height={162.723}
-            priority
-            className="pointer-events-auto w-[40vw] max-w-530 h-auto"
-          />
+          <div className="flex flex-col items-center gap-6 pointer-events-auto">
+            <Image
+              src="/pc_title.png"
+              alt="Photobooth Large"
+              width={529.829}
+              height={162.723}
+              priority
+              className="pointer-events-auto w-[40vw] max-w-530 h-auto"
+            />
+            <Link href="/timer" className="hidden lg:flex xl:hidden relative group">
+              <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-2 translate-y-2 group-hover:bg-[#F2AEBD] transition-colors"></div>
+              <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 lg:py-4 px-6 lg:px-10 group-hover:bg-[#3D568F] group-hover:border-[#F2AEBD] transition-colors">
+                <Image
+                  src="/def_start.png"
+                  alt="Start Button"
+                  width={200}
+                  height={15}
+                  priority
+                  className="pointer-events-auto w-[5vw] sm:w-[5vw] md:w-[10vw] lg:w-[8vw] h-auto group-hover:hidden"
+                />
+                <Image
+                  src="/hovered_start.png"
+                  alt="Start Button"
+                  width={200}
+                  height={15}
+                  priority
+                  className="pointer-events-auto w-[5vw] sm:w-[5vw] md:w-[10vw] lg:w-[8vw] h-auto hidden group-hover:block"
+                />
+              </div>
+            </Link>
+          </div>
         </div>
         <div className="hidden lg:flex xl:hidden fixed inset-y-0 right-25 items-center justify-center pointer-events-none z-10">
           <Image
@@ -53,17 +83,39 @@ export default function Home() {
             className="pointer-events-auto w-[40vw] max-w-554 h-auto"
           />
         </div>
-
         {/* Large Screen Size */}
         <div className="hidden xl:flex fixed inset-y-0 left-25 items-center justify-center pointer-events-none z-10">
-          <Image
-            src="/pc_title.png"
-            alt="Photobooth Large"
-            width={800}
-            height={162.723}
-            priority
-            className="pointer-events-auto w-[40vw] max-w-800 h-auto"
-          />
+          <div className="flex flex-col items-center gap-6 pointer-events-auto">
+            <Image
+              src="/pc_title.png"
+              alt="Photobooth Large"
+              width={800}
+              height={162.723}
+              priority
+              className="pointer-events-auto w-[40vw] max-w-800 h-auto"
+            />
+            <Link href="/timer"className="hidden xl:flex relative group">
+              <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-3 translate-y-3 group-hover:bg-[#F2AEBD] transition-colors"></div>
+              <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 lg:py-6 px-6 lg:px-15 group-hover:bg-[#3D568F] group-hover:border-[#F2AEBD] transition-colors">
+                <Image
+                  src="/def_start.png"
+                  alt="Start Button"
+                  width={200}
+                  height={15}
+                  priority
+                  className="pointer-events-auto w-[5vw] sm:w-[5vw] md:w-[10vw] lg:w-[8vw] h-auto group-hover:hidden"
+                />
+                <Image
+                  src="/hovered_start.png"
+                  alt="Start Button"
+                  width={200}
+                  height={15}
+                  priority
+                  className="pointer-events-auto w-[5vw] sm:w-[5vw] md:w-[10vw] lg:w-[8vw] h-auto hidden group-hover:block"
+                />
+              </div>
+            </Link>
+          </div>
         </div>
         <div className="hidden xl:flex fixed inset-y-0 right-25 items-center justify-center pointer-events-none z-10">
           <Image
