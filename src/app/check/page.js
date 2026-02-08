9fc5de6645f7a9e0d4ded2ff1671b476b813b20a
Rey <reynard.john.rabanal@gmail.com>
2026-02-08 13:01:47 +0800

    check page responsiveness for laptops with small height

--- src/app/check/page.js
+++ src/app/check/page.js
@@ -36,13 +36,13 @@ function RetakeContent() {
       <div className="h-dvh overflow-hidden bg-[#FDFDF5]">
         <Border />
         <main className="flex flex-col overflow-y-auto lg:overflow-hidden xl:overflow-hidden overscroll-none h-full justify-start w-full items-center p-10 sm:p-10 md:p-12 lg:p-24 pt-4 sm:pt-6 md:pt-0 lg:pt-30 xl:pt-20 bg-[#FDFDF5]">
-          <div className="flex mt-5 md:mt-40 lg:-mt-15 xl:mt-0 mb-2 text-center">
+          <div className="flex mt-5 md:mt-40 lg:-mt-20 xl:mt-0 mb-2 text-center">
             <Image
               src="/retake_asset.png"
               alt="Smile"
               width={240}
               height={60}
-              className="mx-auto w-5wv"
+              className="mx-auto w-5wv lg:w-35"
             />
           </div>
           <div
@@ -62,7 +62,7 @@ function RetakeContent() {
                 }`}
               >
                 <div
-                  className={`relative overflow-hidden rounded border-4 border-[#F2AEBD] max-w-[calc(23vh*16/9)] ${shotParam === 3 && index === 2 ? "md:w-46 md:h-46" : ""}`}
+                  className={`relative overflow-hidden rounded border-4 border-[#F2AEBD] max-w-[calc(23vh*16/9)] lg:max-w-[calc(20vh*16/9)] xl:max-w-[calc(23vh*16/9)] ${shotParam === 3 && index === 2 ? "md:w-46 md:h-46" : ""}`}
                 >
                   <img
                     className={`w-full ${
@@ -92,14 +92,14 @@ function RetakeContent() {
             className="relative inline-block group mt-5 mb-5"
           >
             <div className="absolute inset-0 bg-[#3D568F] rounded-xl translate-x-2 translate-y-2 group-hover:bg-[#F2AEBD] transition-colors"></div>
-            <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 xl:py-5 px-30 xl:px-40 group-hover:bg-[#3D568F] group-hover:border-[#F2AEBD] transition-colors">
+            <div className="relative bg-[#F2DDDC] border-2 border-[#3D568F] rounded-xl py-3 lg:py-2 xl:py-5 px-30 xl:px-40 group-hover:bg-[#3D568F] group-hover:border-[#F2AEBD] transition-colors">
               <Image
                 src="/next_button.png"
                 alt="Next Button"
                 width={200}
                 height={15}
                 priority
-                className="pointer-events-none w-[10vw] max-w-15 lg:w-[7vw] xl:w-[3vw] h-auto group-hover:hidden"
+                className="pointer-events-none w-[10vw] max-w-15 lg:w-[4vw] xl:w-[3vw] h-auto group-hover:hidden"
               />
               <Image
                 src="/hover_next.png"
@@ -107,7 +107,7 @@ function RetakeContent() {
                 width={200}
                 height={15}
                 priority
-                className="pointer-events-none w-[10vw] max-w-15 lg:w-[7vw] xl:w-[3vw] h-auto hidden group-hover:block"
+                className="pointer-events-none w-[10vw] max-w-15 lg:w-[4vw] xl:w-[3vw] h-auto hidden group-hover:block"
               />
             </div>
           </Link>
