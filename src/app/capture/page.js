cd2ed94b07387fce45e9161faa67841e160c6953
Rey <reynard.john.rabanal@gmail.com>
2026-02-08 13:58:43 +0800

    Added back button to pages for better UX

--- src/app/capture/page.js
+++ src/app/capture/page.js
@@ -278,7 +278,7 @@ function CaptureContent() {
             onCameraStateChange={handleCameraStateChange}
           />
           <Link href="/select">
-            <div className="absolute top-15 left-15 z-10 w-[30vw] md:w-[20vw] lg:w-[3vw]">
+            <div className="absolute top-10 left-10 lg:top-15 lg:left-15 z-10 w-[5vw] lg:w-[3vw]">
               <Image
                 src="/back_page.png"
                 alt="Bottom Left Decoration"
